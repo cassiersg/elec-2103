2fb8a54bb1f5f58db8ffa96d17e8270808f08b28
Gaëtan Cassiers <gaetan.cassiers@gmail.com>
2017-02-08 13:51:57 +0100

    Cleanup formatting of mtl_touch_controller.sv (untested)

--- sv/mtl_touch_controller.sv
+++ sv/mtl_touch_controller.sv
@@ -1,14 +1,12 @@
-
-
 // --------------------------------------------------------------------
-// Copyright (c) 2007 by Terasic Technologies Inc. 
+// Copyright (c) 2007 by Terasic Technologies Inc.
 // --------------------------------------------------------------------
 //
 // Permission:
 //
 //   Terasic grants permission to use and modify this code for use
-//   in synthesis for all Terasic Development Boards and Altera Development 
-//   Kits made by Terasic.  Other use of this code, including the selling 
+//   in synthesis for all Terasic Development Boards and Altera Development
+//   Kits made by Terasic.  Other use of this code, including the selling
 //   ,duplication, or modification of any portion is strictly prohibited.
 //
 // Disclaimer:
@@ -17,11 +15,11 @@
 //   which illustrates how these types of functions can be implemented.
 //   It is the user's responsibility to verify their design for
 //   consistency and functionality through the use of formal
-//   verification methods.  Terasic provides no warranty regarding the use 
+//   verification methods.  Terasic provides no warranty regarding the use
 //   or functionality of this code.
 //
 // --------------------------------------------------------------------
-//           
+//
 //                     Terasic Technologies Inc
 //                     356 Fu-Shin E. Rd Sec. 1. JhuBei City,
 //                     HsinChu County, Taiwan
@@ -34,22 +32,22 @@
 //
 // Revision History :
 // --------------------------------------------------------------------
-//  Ver  :| Author            	  :| Mod. Date :| Changes Made:
-//  V1.0 :| Johnny Fan			     :| 07/06/30  :| Initial Revision
-//	 V2.0 :| Charlotte Frenkel      :| 14/08/03  :| Adaptation for ELEC2103 project 
-//  V3.0 :| Ludovic Moreau			  :| 17/02/06  :| Adaptation for ELEC2103 project
+//  Ver  :| Author            :| Mod. Date :| Changes Made:
+//  V1.0 :| Johnny Fan          :| 07/06/30  :| Initial Revision
+//  V2.0 :| Charlotte Frenkel :| 14/08/03  :| Adaptation for ELEC2103 project
+//  V3.0 :| Ludovic Moreau    :| 17/02/06  :| Adaptation for ELEC2103 project
 // --------------------------------------------------------------------
 
 module mtl_touch_controller(
-	input 	iCLK,
-	input 	iRST,
-	// MTL TOUCH
-	input    MTL_TOUCH_INT_n,		// Interrupt pin of Touch IC (from MTL)
-	inout 	MTL_TOUCH_I2C_SDA,	// I2C data pin of Touch IC (from/to MTL)
-	output   MTL_TOUCH_I2C_SCL,	// I2C clock pin of Touch IC (from MTL)
-	// Gestures
-	output   Gest_W,					// Decoded gesture (sliding towards West)
-	output   Gest_E					// Decoded gesture (sliding towards East)
+    input     iCLK,
+    input     iRST,
+    // MTL TOUCH
+    input    MTL_TOUCH_INT_n,  // Interrupt pin of Touch IC (from MTL)
+    inout     MTL_TOUCH_I2C_SDA,  // I2C data pin of Touch IC (from/to MTL)
+    output   MTL_TOUCH_I2C_SCL,  // I2C clock pin of Touch IC (from MTL)
+    // Gestures
+    output   Gest_W,  // Decoded gesture (sliding towards West)
+    output   Gest_E  // Decoded gesture (sliding towards East)
 );
 
 //=============================================================================
@@ -60,8 +58,7 @@ logic [9:0] reg_x1, reg_x2, reg_x3, reg_x4, reg_x5;
 logic [8:0] reg_y1, reg_y2, reg_y3, reg_y4, reg_y5;
 logic [1:0] reg_touch_count;
 logic [7:0] reg_gesture;
-logic			touch_ready;
-
+logic touch_ready;
 
 //=============================================================================
 // Structural coding
@@ -76,53 +73,47 @@ logic			touch_ready;
 // section 3.3 of the MTL datasheet available in the project
 // file folder.
 i2c_touch_config  i2c_touch_config_inst (
-	.iCLK(iCLK),
-	.iRSTN(~iRST),
-	.INT_n(~MTL_TOUCH_INT_n),
-	.oREADY(touch_ready),
-	.oREG_X1(reg_x1),								
-	.oREG_Y1(reg_y1),								
-	.oREG_X2(reg_x2),								
-	.oREG_Y2(reg_y2),
-	.oREG_X3(reg_x3),								
-	.oREG_Y3(reg_y3),	
-	.oREG_X4(reg_x4),								
-	.oREG_Y4(reg_y4),
-	.oREG_X5(reg_x5),								
-	.oREG_Y5(reg_y5),
-	.oREG_TOUCH_COUNT(reg_touch_count),								
-	.oREG_GESTURE(reg_gesture),								
-	.I2C_SCLK(MTL_TOUCH_I2C_SCL),
-	.I2C_SDAT(MTL_TOUCH_I2C_SDA)
+    .iCLK(iCLK),
+    .iRSTN(~iRST),
+    .INT_n(~MTL_TOUCH_INT_n),
+    .oREADY(touch_ready),
+    .oREG_X1(reg_x1),
+    .oREG_Y1(reg_y1),
+    .oREG_X2(reg_x2),
+    .oREG_Y2(reg_y2),
+    .oREG_X3(reg_x3),
+    .oREG_Y3(reg_y3),
+    .oREG_X4(reg_x4),
+    .oREG_Y4(reg_y4),
+    .oREG_X5(reg_x5),
+    .oREG_Y5(reg_y5),
+    .oREG_TOUCH_COUNT(reg_touch_count),
+    .oREG_GESTURE(reg_gesture),
+    .I2C_SCLK(MTL_TOUCH_I2C_SCL),
+    .I2C_SDAT(MTL_TOUCH_I2C_SDA)
 );
-
 
 // These two modules are small buffers for the touch
 // controller outputs.
 // The first one is for the sliding "West" gesture,
 // the second one for the sliding "East" gesture.
 // More details are given while defining the module, please see below.
-
-touch_buffer	touch_buffer_west (
-	.clk (iCLK),
-	.rst (iRST),
-	.trigger (touch_ready && (reg_gesture == 8'h1C)),
-	.pulse (Gest_W)
+touch_buffer touch_buffer_west (
+    .clk (iCLK),
+    .rst (iRST),
+    .trigger (touch_ready && (reg_gesture == 8'h1C)),
+    .pulse (Gest_W)
 );
 
-touch_buffer	touch_buffer_east (
-	.clk (iCLK),
-	.rst (iRST),
-	.trigger (touch_ready && (reg_gesture == 8'h14)),
-	.pulse (Gest_E)
+touch_buffer touch_buffer_east (
+    .clk (iCLK),
+    .rst (iRST),
+    .trigger (touch_ready && (reg_gesture == 8'h14)),
+    .pulse (Gest_E)
 );
 
 
 endmodule // mtl_touch_controller
-
-
-///////////////////////////////////////////////////////////////////////////
-
 
 /*
  * This small counting module generates a one-cycle
@@ -134,35 +125,34 @@ endmodule // mtl_touch_controller
  *   the touch controller while giving a fast response,
  * - 0.5 secs is for avoiding to skip two slides with
  *   a single touch.
- */
+*/
 module touch_buffer (
-	input  logic	clk,
-	input  logic	rst,
-	input  logic	trigger,
-	output logic	pulse
-	);
+    input  logic clk,
+    input  logic rst,
+    input  logic trigger,
+    output logic pulse
+);
 
-	logic active;
-	logic [31:0] count;
-	
-	always_ff @ (posedge clk) begin
-	
-		if (rst) begin
-			active <= 1'b0;
-			count <= 32'd0;
-		end else begin
-			if (trigger && !active)
-				active <= 1'b1;
-			else if (active && (count < 32'd25000000))
-				count <= count + 32'b1;
-			else if (count >= 32'd25000000) begin
-				active <= 1'b0;
-				count <= 32'd0;
-			end
-		end
-		
-	end
+logic active;
+logic [31:0] count;
 
-	assign pulse = (count==32'd10000000); 
-	
+always_ff @ (posedge clk) begin
+    if (rst) begin
+        active <= 1'b0;
+        count <= 32'd0;
+    end else begin
+        if (trigger && !active)
+            active <= 1'b1;
+        else if (active && (count < 32'd25000000))
+            count <= count + 32'b1;
+        else if (count >= 32'd25000000) begin
+            active <= 1'b0;
+            count <= 32'd0;
+        end
+    end
+
+end
+
+assign pulse = (count==32'd10000000);
+
 endmodule // touch_buffer
