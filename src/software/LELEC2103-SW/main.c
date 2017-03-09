2586c13ae6794b0c691395e35c7519ca3864766e
Gaëtan Cassiers <gaetan.cassiers@gmail.com>
2017-03-09 22:56:12 +0100

    A better demo in C

--- src/software/LELEC2103-SW/main.c
+++ src/software/LELEC2103-SW/main.c
@@ -13,6 +13,7 @@ OS_STK    task2_stk[TASK_STACKSIZE];
 #define TASK2_PRIORITY      2
 
 #define TILES_ROW 100
+#define GRID_SCALE_FACTOR 4
 
 char grid[8][8] = {
 	{0, 0, 0, 0, 0, 0, 0, 0},
@@ -27,7 +28,6 @@ char grid[8][8] = {
 
 void task1(void* pdata)
 {
-	char grid_reversed;
 	int i, j;
 	
 	volatile char * tiles_ram = (char *) TILE_IMAGE_BASE;
@@ -37,25 +37,26 @@ void task1(void* pdata)
 	for (i=0; i<4*1500; i++) {
 			tiles_idx_ram[i] = 0;
 	}
-	tiles_idx_ram[1000] = 2;
-	tiles_idx_ram[1099] = 4;
-	tiles_idx_ram[1100] = 4;
-	tiles_idx_ram[1201] = 4;
+//	tiles_idx_ram[1000] = 2;
+//	tiles_idx_ram[1099] = 4;
+//	tiles_idx_ram[1100] = 4;
+//	tiles_idx_ram[1201] = 4;
 	for (i=0; i<8; i++) {
 		for (j=0; j<8; j++) {
-			grid_reversed = grid[j][i] ? 1 : 0;
-			tiles_idx_ram[j*TILES_ROW+i*2] = grid_reversed;
-			tiles_idx_ram[j*TILES_ROW+(i*2)+1] = grid_reversed;
-			//tiles_idx_ram[(1+j)*TILES_ROW+(i*2)] = grid_reversed;
-			//tiles_idx_ram[(1+j)*TILES_ROW+(i*2)+1] = grid_reversed;
+			int i_off, j_off;
+			for (i_off = 0; i_off < GRID_SCALE_FACTOR; i_off++) {
+				for (j_off = 0; j_off < GRID_SCALE_FACTOR; j_off++) {
+					tiles_idx_ram[TILES_ROW*(j*GRID_SCALE_FACTOR+j_off)+i*GRID_SCALE_FACTOR+i_off] = grid[j][i];
+				}
+			}
 		}
 	}
 	for (i=0; i<64; i++) {
 		for (j=0; j<4; j++) {
 			char tidx;
-			if (i==0) tidx = 3;
+			//if (i==0) tidx = 3;
 			//else if ((i & 0x7)==0) tidx = 2;
-			else tidx = j;
+			tidx = j;
 			tiles_ram[i+j*64] = tidx;
 		}
 		tiles_ram[4*64+i] = (i & 0x3);
