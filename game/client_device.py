32cd04ba3bd8b2e198e9280c92ed865084b77c76
Gaëtan Cassiers <gaetan.cassiers@gmail.com>
2017-04-16 11:04:29 +0000

    Smoother client
    threaded display
    round_gauge interpolation based on round_gauge_speed
    optimization

--- game/client_device.py
+++ game/client_device.py
@@ -1,3 +1,4 @@
+import time
 from time import sleep
 import spidev
 import struct
@@ -34,8 +35,8 @@ def read_spi(spi, address, nb_words=1):
     return res[8:]
 
 def set_display(spi, display_id):
-    write_spi(spi, 0x0, [0x00, 0x00, 0x00, display_id])
-    while bytes2int(read_spi(spi, 0x1)) != display_id:
+    write_spi(spi, 0x30000, [0x00, 0x00, 0x00, display_id])
+    while bytes2int(read_spi(spi, 0x30000)) != display_id:
         sleep(0.01)
 
 class HardwareInterface:
@@ -51,11 +52,18 @@ class HardwareInterface:
         self.current_display = new_display
 
     def send_spi_buf(self, buf):
+        t0 = time.time()
+        assert(len(buf) <= 12000)
         idx = 0
+        t1 = time.time()
         while buf:
             sent_buf, buf = buf[:4000], buf[4000:]
-            write_spi(self.spi, idx, sent_buf)
+            write_spi(self.spi, 0x10000 + idx, sent_buf)
             idx += 1000
+        t2 = time.time()
+        self.pageflip()
+        t3 = time.time()
+        print('SPI: init', t1-t0, 'write', t2-t1, 'pageflip', t3-t2, 'n writes', idx//1000)
 
     def draw_tiles(self, tiles_idx):
         assert len(tiles_idx) == 6000
@@ -80,7 +88,7 @@ class HardwareInterface:
             write_spi(self.spi, 0x02, 4*[0x00])
 
         cur_acc_value = min(255, max(0, (bytes2int(read_spi(self.spi, 0x03)) + 256) // 2))
-        print(cur_acc_value)
+        #print(cur_acc_value)
 
         return (False, cur_acc_value, events)
 
