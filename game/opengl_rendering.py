0ed59708408e0a2534673ce155d542ca21f408bc
Gaëtan Cassiers <gaetan.cassiers@gmail.com>
2017-04-17 21:29:43 +0200

    fix small typo

--- game/opengl_rendering.py
+++ game/opengl_rendering.py
@@ -1,6 +1,7 @@
 
 import game_global as gg
 import opengl.cubes as cubes
+import opengl.compression as compression
 import pygame
 import pickle
 import random
@@ -29,10 +30,13 @@ class Renderer:
         grid = bytearray(x for y in grid for x in y)
         p1x, p1y, p2x, p2y = players_xy
         cubes.draw_cubes(grid, 7, 15, p1x, p1y, p2x, p2y, player_id, round_gauge)
-        buf = bytearray(cubes.width*cubes.height*4)
-        cubes.cubes_image_export(buf)
+        pixel_buf = bytearray(cubes.width*cubes.height*4)
+        cubes.cubes_image_export(pixel_buf)
+        #compressed_buf = bytearray(len(pixel_buf))
+        #n_chunks, compressed_size = compression.chunk_compress_huffman(
+        #    pixel_buf, compressed_buf, 32)
         s = self.hw_interface.screen
         b = s.get_buffer()
-        b.write(bytes(buf))
+        b.write(bytes(pixel_buf))
         pygame.display.flip()
 
