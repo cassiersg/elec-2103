912376b6eb01845dc0a7c473c03cfac1c867634f
Gaëtan Cassiers <gaetan.cassiers@gmail.com>
2017-04-20 21:30:02 +0200

    cleanup -- note: pixel readback not working

--- src/rpi_mmu.sv
+++ src/rpi_mmu.sv
@@ -27,25 +27,21 @@ assign chip  = rpi_addr[23:16];
 
 assign message_addr = rpi_addr[15:0];
 assign buf_addr = rpi_addr[15:0];
-//assign buf1_addr = rpi_addr[15:0];
 
 always_comb
 case (chip)
 	8'h00: rpi_rd = message_rd;
 	8'h01: rpi_rd = buf_rd;
-//	8'h02: rpi_rd = buf1_rd;
 	8'h03: rpi_rd = display_ctrl_rd;
 	default: rpi_rd = 32'b0;
 endcase
 
 assign message_wd = rpi_wd;
 assign buf_wd = rpi_wd;
-//assign buf1_wd = rpi_wd;
 assign display_ctrl_wd = rpi_wd;
 
 assign message_we = (chip == 8'h00) & rpi_we;
 assign buf_we = (chip == 8'h01) & rpi_we;
-//assign buf1_we = (chip == 8'h02) & rpi_we;
 assign display_ctrl_we = (chip == 8'h03) & rpi_we;
 
 assign message_re = (chip == 8'h00) & rpi_re;
