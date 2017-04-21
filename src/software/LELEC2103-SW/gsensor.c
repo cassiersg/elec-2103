76bc010e67596ebb8827f3af5ba17d3f72bd2ca4
Gaëtan Cassiers <gaetan.cassiers@gmail.com>
2017-04-21 13:21:35 +0200

    Fix gsensor sign reading bug + cleanup main

--- src/software/LELEC2103-SW/gsensor.c
+++ src/software/LELEC2103-SW/gsensor.c
@@ -12,12 +12,13 @@ void task_g_sense(void *pdata) {
 	volatile int* msg_reg = (int *) MESSAGE_MEM_BASE;
 
 	assert(ADXL345_SPI_Init(SPI_GSENSOR_BASE));
-	OSTimeDlyHMSM(0, 0, 0, 100);
+	OSTimeDlyHMSM(0, 0, 1, 0);
 	while (1) {
-		alt_u16 szXYZ[3];
+		alt_16 szXYZ[3];
 		assert(ADXL345_SPI_IsDataReady(SPI_GSENSOR_BASE));
 		assert(ADXL345_SPI_XYZ_Read(SPI_GSENSOR_BASE, szXYZ));
 		msg_reg[3] = szXYZ[1];
-		OSTimeDlyHMSM(0, 0, 0, 100);
+		printf("acc: %i\n", msg_reg[3]);
+		OSTimeDlyHMSM(0, 0, 0, 50);
 	}
 }
--- src/software/LELEC2103-SW/main.c
+++ src/software/LELEC2103-SW/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include "HAL/inc/priv/alt_iic_isr_register.h"
+#include "altera_avalon_pio_regs.h"
+#include "io.h"
 #include "includes.h"
 #include "system.h"
 #include "mtltouch.h"
 #include "gsensor.h"
-#include "tiling_roms.h"
-
+//#include "tiling_roms.h"
+#include "compressed.h"
 
 /* Definition of Task Stacks */
 #define   TASK_STACKSIZE       2048
@@ -19,67 +22,41 @@ OS_STK    task3_stk[TASK_STACKSIZE];
 #define TASK2_PRIORITY      6
 #define TASK3_PRIORITY      8
 
-#define WHITE	0x000000
-#define BLACK	0xFFFFFF
-#define RED		0xFF0000
-#define GREEN	0x00FF00
-#define GRAY	0x808080
+
 
 #define DEBUG	1
 
-void init_color_map() {
-	volatile int * colormap_ram = (int *) COLORMAP_BASE;
-        assert(COLORMAP_ROM_LEN == 256);
-        memcpy(colormap_ram, colormap_rom, 256*sizeof(int));
-}
-
-void init_tiles_idx_ram() {
-	volatile char * tiles_idx_0_ram = (char *) TILE_IDX_0_BASE;
-	volatile char * tiles_idx_1_ram = (char *) TILE_IDX_1_BASE;
-
-	int i;
-	for (i=0; i< 4*1500; i++) {
-		tiles_idx_0_ram[i] = 0;
-		tiles_idx_1_ram[i] = 0;
-	}
-}
-
-void init_tiles_ram() {
-	volatile char * tiles_ram = (char *) TILE_IMAGE_BASE;
-        assert(TILES_IMAGE_ROM_LEN <= 256); // FIXME replace with constant from system.h
-        memcpy(tiles_ram, tiles_image_rom, TILES_IMAGE_ROM_LEN);
-}
 
 void init() {
-	init_color_map();
-	init_tiles_idx_ram();
-	init_tiles_ram();
+
 }
 
 void task1(void* pdata) {
-	volatile int * display_control = (int *) DISPLAY_CONTROL_BASE;
-	volatile int* msg_reg = (int *) MESSAGE_MEM_BASE;
-	
-	*display_control = 1;
-	msg_reg[1] = *display_control;
-
-//	assert(ADXL345_SPI_Init(SPI_GSENSOR_BASE));
-	while (1) {
-		*display_control = msg_reg[0];
-		msg_reg[1] = *display_control;
-
-		OSTimeDlyHMSM(0, 0, 0, 20);
+//	volatile int* msg_reg = (int *) MESSAGE_MEM_BASE;
 //
-//		OSTimeDlyHMSM(0, 0, 1, 0);
-//		alt_16 szXYZ[3];
-//		bool ready = ADXL345_SPI_IsDataReady(SPI_GSENSOR_BASE);
-//		bool readres = ADXL345_SPI_XYZ_Read(SPI_GSENSOR_BASE, szXYZ);
-//		printf("data ready: %d, x: %d, y: %d, z: %d, res: %d\n", ready, szXYZ[0], szXYZ[1], szXYZ[2], readres);
-
+//
+//
+	printf("start task 1\n");
+	while (1) {
+		OSTimeDlyHMSM(0, 0, 0, 20);
 	}
 }
 
+//void endframe_isr(void* data) {
+//	IOWR(PIO_ENDFRAME_BASE, 3, 1);
+//}
+
 int main(void) {
+//	need_pageflip = 0;
+//	IOWR(PIO_ENDFRAME_BASE, 2, 1);
+//	 int reg_ret = alt_iic_isr_register(PIO_ENDFRAME_IRQ_INTERRUPT_CONTROLLER_ID,
+//			 PIO_ENDFRAME_IRQ,
+//			 &endframe_isr,
+//			 NULL,
+//			 NULL);
+//	 assert(!reg_ret);
+
+
 	init();
 
 	OSTaskCreateExt(task1,
@@ -92,6 +69,7 @@ int main(void) {
 			NULL,
 			0);
 
+	printf("started task 1\n");
 	OSTaskCreateExt(task_touch_sense,
 			NULL,
 			(void *)&task2_stk[TASK_STACKSIZE-1],
