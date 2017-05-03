56a48e84531de1abba44f34e0e9d7624ec007837
Gaëtan Cassiers <gaetan.cassiers@gmail.com>
2017-05-03 09:16:41 +0200

    fix cont_run.sh

--- startup/cont_run.sh
+++ startup/cont_run.sh
@@ -7,12 +7,14 @@ cd ~/elec-2103/startup
 
 if [ $HOSTNAME = "raspberrypi-g1-2" ]; then
     echo "Launching continuous client"
-    ./cont_client.sh
+    ./cont_client.sh &
 elif [ $HOSTNAME = "raspberrypi-g1-1" ]; then
     echo "Launching continuous game server"
     ./cont_server.sh &
     echo "Launching continuous web server"
-    ./cont_web_server.sh
+    ./cont_web_server.sh &
+    echo "Launching continuous client"
+    ./cont_client.sh &
 else
     echo "Not on a Raspberry Pi, why would you launch this script?"
 fi
